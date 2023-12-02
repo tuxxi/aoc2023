c2040b732c41a529d15c1b12ace55b26692ca43d
Aidan Sojourner <aidan@sojourner.me>
2023-12-02 11:51:58 -0800

    day4

---
+++ days/day04.go
@@ -0,0 +1,64 @@
+package days
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/tuxxi/aoc2023/util"
+	"golang.org/x/exp/slices"
+)
+
+func init() {
+	util.RegisterDay(4, day4)
+}
+
+func day4(input []string) (any, any) {
+	var part1, part2 int
+
+	cardCounts := map[int]int{}
+	// each card has one original copy
+	for i := range input {
+		cardCounts[i+1] = 1
+	}
+
+	for i, line := range input {
+		l := strings.Split(line, ":")[1]
+		l2 := strings.Split(l, "|")
+		winning := NumsToInts(strings.Fields(l2[0]))
+		have := NumsToInts(strings.Fields(l2[1]))
+
+		matching := 0
+		for _, h := range have {
+			if slices.Contains(winning, h) {
+				matching++
+			}
+		}
+		if matching != 0 {
+			part1 += int(math.Exp2(float64(matching - 1)))
+		}
+
+		cardNum := i + 1
+		// next N cards are also winning
+		for n := cardNum + 1; n <= cardNum+matching; n++ {
+			copies := cardCounts[cardNum]
+			cardCounts[n] += copies
+		}
+	}
+	for _, count := range cardCounts {
+		part2 += count
+	}
+	return part1, part2
+}
+
+func NumsToInts(nums []string) []int {
+	var ret []int
+	for _, num := range nums {
+		i, err := strconv.Atoi(num)
+		if err != nil {
+			panic(err)
+		}
+		ret = append(ret, i)
+	}
+	return ret
+}
---
+++ days/day04_test.go
@@ -0,0 +1,21 @@
+package days
+
+import (
+	"testing"
+
+	"github.com/tuxxi/aoc2023/util"
+)
+
+func TestD4(t *testing.T) {
+	input := []string{
+		"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+		"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+		"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+		"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+		"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+	}
+	p1, p2 := day4(input)
+	util.AssertEq(t, p1, 13)
+	util.AssertEq(t, p2, 30)
+}
